9d8eb37916e89bbeef9cb83465d92578a36aca6e
adnan159 <osmanhaider159@gmail.com>
2023-02-21 23:24:49 +0600

    init

---
+++ guten-magic.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Guten Magic
+ * Plugin URI: https://wordpress.org
+ * Description: Guten Magic a plugin for WordPress that allows you to easily create custom content layouts.
+ * Version: 1.0.0
+ * Author: Adnan
+ * Author URI: https://osmanhaideradnan.wordpress.com/
+ * Author URI:
+ * Text Domain: guten-magic
+ * Requires at least: 5.9
+ * Requires PHP: 7.2
+ *
+ */
